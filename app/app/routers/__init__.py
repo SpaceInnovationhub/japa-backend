73bd981443e1c0ad4182c95ee22ffa2554031e5c
Prince Okoko <86599866+SpaceInnovationhub@users.noreply.github.com>
2026-02-16 11:05:42 +0100

    Ready for Production

--- app/app/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file makes the routers directory a Python package
-from .user import router as user_router
-from .embassy import router as embassy_router
-from .tickets import router as tickets_router
-from .evacuation import router as evacuation_router
-
-__all__ = ["user_router", "embassy_router", "tickets_router", "evacuation_router"]
