6a3a620ef6907fbd15a3b1f519595c911e45da13
Prince Okoko <86599866+SpaceInnovationhub@users.noreply.github.com>
2026-03-18 21:43:05 +0100

    fix __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from .user import router as user_router
+from .embassy import router as embassy_router
+from .tickets import router as tickets_router
+from .evacuation import router as evacuation_router
+
+__all__ = ["user_router", "embassy_router", "tickets_router", "evacuation_router"]
