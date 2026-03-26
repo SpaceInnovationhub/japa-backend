022d19054d23eb57e65649a822d1dbfe7af3a22b
Prince Okoko <86599866+SpaceInnovationhub@users.noreply.github.com>
2026-03-26 16:06:37 +0100

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class SignupRequest(BaseModel):
+    fullname: str
+    email: str
+    password: str
+
+@app.get("/")
+def root():
+    return {"message": "JAPA API is running!"}
+
+@app.post("/signup")
+def signup(request: SignupRequest):
+    return {
+        "message": "User created successfully",
+        "user": request
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("test_main:app", host="0.0.0.0", port=8000, reload=True)
